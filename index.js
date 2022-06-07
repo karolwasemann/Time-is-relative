6e8f1e7eb6493ddee9e876d0823b80248d38e34c
karolwasemann <kw.kwaq@gmail.com>
2022-06-07 11:50:07 +0200

    multiplier

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 // Import stylesheets
 import './style.css';
 
+let time = document.getElementById('time');
+const multiplierButtons = document.querySelectorAll('[data-action');
+
+function setTime() {
+  let spentSec = parseInt(time.value) + 1;
+  time.value = spentSec;
+}
+
+function setTimer() {
+  let speed = 1000;
+  speed = multiplier(speed);
+  console.log(speed);
+  return setInterval(setTime, speed);
+}
+
+function multiplier(speed) {
+  const multiField = document.querySelector('#multi');
+  multiplierButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      if (e.target.getAttribute('data-action') === 'decrement') {
+        multiField.value = parseInt(multi.value) - 1;
+      } else {
+        multiField.value = parseInt(multi.value) + 1;
+      }
+    });
+  });
+
+  speed = 2000;
+  return speed;
+}
+
+setTimer();
