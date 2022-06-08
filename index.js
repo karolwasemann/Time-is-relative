b6f9dc82ac32418fe40ab66792cab0df8f3dd53a
karolwasemann <kw.kwaq@gmail.com>
2022-06-08 08:52:11 +0200

    started multplier function

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 import './style.css';
 
 let time = document.getElementById('time');
-const multiplierButtons = document.querySelectorAll('[data-action');
-const stopBtn = document.querySelector('[data-play="stop"]');
-const startBtn = document.querySelector('[data-play="start"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const startBtn = document.querySelector('[data-action="start"]');
 
 let isRunning = true;
 let speed = 1000;
 
+decrementBtn.addEventListener('click', () => multiplier(false, speed));
+incrementBtn.addEventListener('click', () => multiplier(true, speed));
+
 stopBtn.addEventListener('click', () => (isRunning = false));
 startBtn.addEventListener('click', () => (isRunning = true));
-speed = multiplier(speed);
+// speed = multiplier(speed);
 
 setInterval(() => {
   if (isRunning) {
@@ -20,18 +24,23 @@ setInterval(() => {
   }
 }, speed);
 
-function multiplier(speed) {
+function multiplier(isIncerement, speed) {
   const multiField = document.querySelector('#multi');
-  multiplierButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      if (e.target.getAttribute('data-action') === 'decrement') {
-        multiField.value = parseInt(multi.value) - 1;
-      } else {
-        multiField.value = parseInt(multi.value) + 1;
-      }
-    });
-  });
+  let value = parseInt(multiField.value);
+
+  if (isIncerement) {
+    value += 1;
+  } else {
+    value -= 1;
+  }
+  if (value === -1) {
+    value = 2;
+  }
+  if (value === 1) {
+    value = -2;
+  }
 
   speed = 1000;
+  multiField.value = value;
   return speed;
 }
