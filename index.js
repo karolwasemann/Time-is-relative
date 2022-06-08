ab3073ee4945d7a97471a2d3b9c345e651faa5c1
karolwasemann <kw.kwaq@gmail.com>
2022-06-08 12:02:19 +0200

    Task solved

--- index.js
+++ index.js
@@ -7,40 +7,72 @@ const incrementBtn = document.querySelector('[data-action="increment"]');
 const stopBtn = document.querySelector('[data-action="stop"]');
 const startBtn = document.querySelector('[data-action="start"]');
 
+let timer;
 let isRunning = true;
-let speed = 1000;
+let interval = 1000;
+let multiplier = 0;
 
-decrementBtn.addEventListener('click', () => multiplier(false, speed));
-incrementBtn.addEventListener('click', () => multiplier(true, speed));
+decrementBtn.addEventListener('click', () => handleMultiplier('decrement'));
+incrementBtn.addEventListener('click', () => handleMultiplier('increment'));
+stopBtn.addEventListener('click', () => {
+  isRunning = false;
+  clearInterval(timer);
+});
+startBtn.addEventListener('click', () => {
+  if (isRunning) return;
+  countTime(interval);
+});
 
-stopBtn.addEventListener('click', () => (isRunning = false));
-startBtn.addEventListener('click', () => (isRunning = true));
-// speed = multiplier(speed);
+countTime(interval);
 
-setInterval(() => {
-  if (isRunning) {
+function handleMultiplier(btnAction) {
+  clearInterval(timer);
+  isRunning = false;
+  handleMultiplierValue(btnAction);
+  document.querySelector('#multi').value = multiplier;
+  interval = calcInverval(multiplier);
+  countTime(interval);
+}
+
+function calcInverval(multi) {
+  if (multi > 0) {
+    return 1000 / multi;
+  }
+  return 1000 * Math.abs(multi);
+}
+
+function handleMultiplierValue(btnAction) {
+  switch (btnAction) {
+    case 'increment':
+      if (multiplier === 0) {
+        multiplier += 1;
+      }
+      if (multiplier === 1) {
+        multiplier = 2;
+      } else if (multiplier === -2) {
+        multiplier = 2;
+      } else {
+        multiplier += 1;
+      }
+      break;
+    case 'decrement':
+      if (multiplier === 0) {
+        multiplier -= 1;
+      }
+      if (multiplier === -1) {
+        multiplier = -2;
+      } else if (multiplier === 2) {
+        multiplier = -2;
+      } else {
+        multiplier -= 1;
+      }
+  }
+}
+
+function countTime(interval) {
+  isRunning = true;
+  timer = setInterval(() => {
     let spentSec = parseInt(time.value) + 1;
     time.value = spentSec;
-  }
-}, speed);
-
-function multiplier(isIncerement, speed) {
-  const multiField = document.querySelector('#multi');
-  let value = parseInt(multiField.value);
-
-  if (isIncerement) {
-    value += 1;
-  } else {
-    value -= 1;
-  }
-  if (value === -1) {
-    value = 2;
-  }
-  if (value === 1) {
-    value = -2;
-  }
-
-  speed = 1000;
-  multiField.value = value;
-  return speed;
+  }, interval);
 }
